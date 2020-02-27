1c2bf4e8dcdf19022a29ab04d944105cbace88bf
romaine murray <romainemurray6@gmail.com>
2020-02-27 18:14:54 -0500

    Step 8 completed

--- app/views.py
+++ app/views.py
@@ -10,6 +10,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
+from werkzeug.security import check_password_hash
 
 
 ###
@@ -31,23 +32,31 @@ def about():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
+    if request.method == "POST" and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
             # Get the username and password values from the form.
-
+        username = form.username.data
+        password = form.password.data
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
             # You will need to import the appropriate function to do so.
             # Then store the result of that query to a `user` variable so it can be
             # passed to the login_user() method below.
+        user = UserProfile.query.filter_by(username=username).first()
+
+        if user is not None and check_password_hash(user.password,password):
 
             # get user id, load into session
             login_user(user)
 
+            
+
             # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            flash('Logged in successfully.','success')
+            return redirect(url_for("secure-page"))  # they should be redirected to a secure-page route instead
+        else:
+            flash('Username or Password is incorrect.','danger')
     return render_template("login.html", form=form)
 
 
